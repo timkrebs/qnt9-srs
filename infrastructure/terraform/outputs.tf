70742d9f1174df50dbb16e4773e5bcc4e91e9bed
Tim Krebs <tim.krebs@hashicorp.com>
2025-10-21 15:57:37 +0200

    Remove Datadog monitoring outputs from Terraform configuration

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -20,19 +20,3 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
-
-# Datadog Monitoring Outputs
-output "datadog_agent_status" {
-  description = "Command to check Datadog agent status"
-  value       = "kubectl get datadogagent datadog -n default"
-}
-
-output "datadog_pods_status" {
-  description = "Command to check Datadog pods"
-  value       = "kubectl get pods -n default -l app.kubernetes.io/component=agent"
-}
-
-output "datadog_site" {
-  description = "Datadog site URL"
-  value       = "https://us3.datadoghq.com"
-}
