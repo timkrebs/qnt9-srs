e624ec7f607d56e89278a6b46828723abebaf39c
Tim Krebs <tim.krebs@hashicorp.com>
2025-10-28 20:53:22 +0100

    fix: Restore ACR admin username and password outputs for better access management

--- infrastructure/terraform/modules/acr/outputs.tf
+++ infrastructure/terraform/modules/acr/outputs.tf
@@ -13,17 +13,17 @@ output "acr_login_server" {
   value       = azurerm_container_registry.acr.login_server
 }
 
-#output "acr_admin_username" {
-#  description = "Admin username for ACR"
-#  value       = azurerm_container_registry.acr.admin_username
-#  sensitive   = true
-#}
-#
-#output "acr_admin_password" {
-#  description = "Admin password for ACR"
-#  value       = azurerm_container_registry.acr.admin_password
-#  sensitive   = true
-#}
+output "acr_admin_username" {
+  description = "Admin username for ACR"
+  value       = azurerm_container_registry.acr.admin_username
+  sensitive   = true
+}
+
+output "acr_admin_password" {
+  description = "Admin password for ACR"
+  value       = azurerm_container_registry.acr.admin_password
+  sensitive   = true
+}
 
 output "acr_identity_principal_id" {
   description = "Principal ID of the ACR managed identity"
